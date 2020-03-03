b2fcf60220b8ecb39e6bc1248ecad6ecb4b6ec91
sesegarb <seseg.arbalzhinova@gmail.com>
2020-03-03 14:19:59 +0000

    week2

--- mobdev-ionic-intro/src/app/app-routing.module.ts
+++ mobdev-ionic-intro/src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-];
-
+import {
+  NgModule
+} from '@angular/core';
+import {
+  PreloadAllModules,
+  RouterModule,
+  Routes
+} from '@angular/router';
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+}];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
