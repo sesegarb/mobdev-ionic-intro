53e709747818b4ba5a15112c2e83496f453bb8df
sesegarb <seseg.arbalzhinova@gmail.com>
2020-03-10 15:26:30 +0000

    week2

--- mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-film-details',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmDetailsPage implements OnInit {
 
-  constructor() { }
+  film: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.co/api/films/${id}`).subscribe(res => {
+      this.film = res;
+    });
   }
 
 }
